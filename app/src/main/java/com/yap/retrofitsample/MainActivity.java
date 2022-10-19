9555c8c41bcf1e457f47682f70fd6425deed4e44
YanAungPhyo <yanaungphyo690@gmail.com>
2022-10-19 10:30:34 +0630

    Retrofit Sample

--- app/src/main/java/com/yap/retrofitsample/MainActivity.java
+++ app/src/main/java/com/yap/retrofitsample/MainActivity.java
@@ -5,12 +5,13 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.yap.retrofitsample.retrofit.Datum;
 import com.yap.retrofitsample.retrofit.Model;
 import com.yap.retrofitsample.retrofit.RetrofitApi;
 import com.yap.retrofitsample.retrofit.RetrofitClient;
 
 
-import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,6 +20,11 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     //github မှာ android studio နဲ့ project တင်နည်း  https://youtu.be/Tzfen6zKKQU
+    //github မှာ android studio နဲ့ file update commit လုပ်နည်း  https://youtu.be/1RpW5DPzALg
+
+    //jsonview chrome extension
+
+    //server က json formatကို android model classဖြစ်တဲ့ pojoကိုပြောင်းချင်ရင် ဒီ siteမှာ ဝင်ပြောင်းပါ  **https://www.jsonschema2pojo.org/**
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +43,10 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<Model> call, Response<Model> response) {
 
 
-                ArrayList<Model.data> data = response.body().getData();
-                for (Model.data data1 : data) {
+                List<Datum> data = response.body().getData();
+                for (Datum data1 : data) {
                     Log.d("CHECKRESPONSE", data1.getEmail());
+                    Log.d("CHECKRESPONSE",data1.getAvatar());
                 }
             }
 
--- app/src/main/java/com/yap/retrofitsample/retrofit/Model.java
+++ app/src/main/java/com/yap/retrofitsample/retrofit/Model.java
@@ -1,103 +1,175 @@
 package com.yap.retrofitsample.retrofit;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Model {
-    String page;
-    String per_page;
-    String total;
-    String total_pages;
-    ArrayList<data>  data;
 
-    public String getPage() {
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("per_page")
+    @Expose
+    private Integer perPage;
+    @SerializedName("total")
+    @Expose
+    private Integer total;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("data")
+    @Expose
+    private List<Datum> data = null;
+    @SerializedName("support")
+    @Expose
+    private Support support;
+
+    public Integer getPage() {
         return page;
     }
 
-    public void setPage(String page) {
+    public void setPage(Integer page) {
         this.page = page;
     }
 
-    public String getPer_page() {
-        return per_page;
+    public Integer getPerPage() {
+        return perPage;
     }
 
-    public void setPer_page(String per_page) {
-        this.per_page = per_page;
+    public void setPerPage(Integer perPage) {
+        this.perPage = perPage;
     }
 
-    public String getTotal() {
+    public Integer getTotal() {
         return total;
     }
 
-    public void setTotal(String total) {
+    public void setTotal(Integer total) {
         this.total = total;
     }
 
-    public String getTotal_pages() {
-        return total_pages;
+    public Integer getTotalPages() {
+        return totalPages;
     }
 
-    public void setTotal_pages(String total_pages) {
-        this.total_pages = total_pages;
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
     }
 
-    public ArrayList<Model.data> getData() {
+    public List<Datum> getData() {
         return data;
     }
 
-    public void setData(ArrayList<Model.data> data) {
+    public void setData(List<Datum> data) {
         this.data = data;
     }
 
-
-    //for array object of data
-    public class data {
-        String id;
-        String email;
-        String first_name;
-        String last_name;
-        String avatar;
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getFirst_name() {
-            return first_name;
-        }
-
-        public void setFirst_name(String first_name) {
-            this.first_name = first_name;
-        }
-
-        public String getLast_name() {
-            return last_name;
-        }
-
-        public void setLast_name(String last_name) {
-            this.last_name = last_name;
-        }
-
-        public String getAvatar() {
-            return avatar;
-        }
-
-        public void setAvatar(String avatar) {
-            this.avatar = avatar;
-        }
+    public Support getSupport() {
+        return support;
     }
+
+    public void setSupport(Support support) {
+        this.support = support;
+    }
+
+//    String page;
+//    String per_page;
+//    String total;
+//    String total_pages;
+//    ArrayList<data>  data;
+//
+//    public String getPage() {
+//        return page;
+//    }
+//
+//    public void setPage(String page) {
+//        this.page = page;
+//    }
+//
+//    public String getPer_page() {
+//        return per_page;
+//    }
+//
+//    public void setPer_page(String per_page) {
+//        this.per_page = per_page;
+//    }
+//
+//    public String getTotal() {
+//        return total;
+//    }
+//
+//    public void setTotal(String total) {
+//        this.total = total;
+//    }
+//
+//    public String getTotal_pages() {
+//        return total_pages;
+//    }
+//
+//    public void setTotal_pages(String total_pages) {
+//        this.total_pages = total_pages;
+//    }
+//
+//    public ArrayList<Model.data> getData() {
+//        return data;
+//    }
+//
+//    public void setData(ArrayList<Model.data> data) {
+//        this.data = data;
+//    }
+//
+//
+//    //for array object of data
+//    public class data {
+//        String id;
+//        String email;
+//        String first_name;
+//        String last_name;
+//        String avatar;
+//
+//        public String getId() {
+//            return id;
+//        }
+//
+//        public void setId(String id) {
+//            this.id = id;
+//        }
+//
+//        public String getEmail() {
+//            return email;
+//        }
+//
+//        public void setEmail(String email) {
+//            this.email = email;
+//        }
+//
+//        public String getFirst_name() {
+//            return first_name;
+//        }
+//
+//        public void setFirst_name(String first_name) {
+//            this.first_name = first_name;
+//        }
+//
+//        public String getLast_name() {
+//            return last_name;
+//        }
+//
+//        public void setLast_name(String last_name) {
+//            this.last_name = last_name;
+//        }
+//
+//        public String getAvatar() {
+//            return avatar;
+//        }
+//
+//        public void setAvatar(String avatar) {
+//            this.avatar = avatar;
+//        }
+//    }
 }
 
 
